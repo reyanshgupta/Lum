cc079118c3b9d4e5d53a23fd9b4e9a6a1a5e12fb
Reyansh Gupta <happygupta@aol.in>
2024-01-07 00:56:28 +0530

    basic functioning

--- app.py
+++ app.py
@@ -1,23 +1,78 @@
 from flask import Flask, render_template, Response
 from camera import Video
+from collections import Counter
 
-app=Flask(__name__)
+app = Flask(__name__)
+
+# Global variable to store emotions
+emotion_counter = Counter()
+
+# Emotion to Spotify playlist embed mapping
+playlist_mapping = {
+    "Happy": [
+        '''<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1EIgBnQZVoxcWq?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>''',
+        '''<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DXaXB8fQg7xif?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>''',
+        '''<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1EIhsvAFc3pIY4?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'''
+    ],
+    "Sad": [
+        '''<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/2U6SPbVuvqX0BSWWylG7ps?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>''',
+        '''<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/6b3EXmoElf2UlxuldGjhRX?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>''',
+        '''<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DWX83CujKHHOn?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'''
+    ],
+    "Angry": [
+        '''<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1EIetewBshGEPK?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>''',
+        '''<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DX9qNs32fujYe?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>''',
+        '''<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1E4AInqF1aqV85?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'''
+    ],
+    "Neutral": [
+        '''<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1EVHGWrwldPRtj?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'''
+        '''<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DX4PP3DA4J0N8?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'''
+        '''<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1EIcJQdzRPklXj?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'''
+    ],
+    "Fearful": [
+        '''<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DX1s9knjP51Oa?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>''',
+        '''<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DX4sWSpwq3LiO?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>''',
+        '''<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1EIfPyixpQG2Dl?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'''
+    ],
+    "Disgusted": [
+        '''<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1EIefLxrHQP8p4?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'''
+        '''<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1EIdcKjz0pIYET?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'''
+        '''<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1EIcqNFLKnDOcZ?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'''
+    ],
+    
+    "Surprised": [
+        '''<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DXbVhgADFy3im?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'''
+        '''<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZEVXbqtItH1ORF8f?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'''
+        '''<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DXa41CMuUARjl?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'''
+    ]
+}
 
 @app.route('/')
 def index():
+    global emotion_counter
+    emotion_counter.clear()
     return render_template('index.html')
 
 def gen(camera):
+    global emotion_counter
     while True:
-        frame = camera.get_frame()
-        yield(b'--frame\r\n'
-              b'Content-Type: image/jpeg\r\n\r\n' + frame + 
-              b'\r\n\r\n')
+        frame, emotion = camera.get_frame()
+        if emotion:
+            emotion_counter[emotion] += 1
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + 
+               b'\r\n\r\n')
 
 @app.route('/muserec')
-
 def video():
     return Response(gen(Video()),
-    mimetype='multipart/x-mixed-replace; boundary=frame')
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
-app.run(debug=True)
+@app.route('/suggest_playlist')
+def suggest_playlist():
+    most_common_emotion = emotion_counter.most_common(1)[0][0] if emotion_counter else 'Neutral'
+    suggested_playlists = playlist_mapping.get(most_common_emotion, [""])
+    return render_template('playlist.html', playlists=suggested_playlists, emotion=most_common_emotion)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- camera.py
+++ camera.py
@@ -7,40 +7,42 @@ face_detector = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_defa
 class Video(object):
     def __init__(self):
         self.video = cv2.VideoCapture(0)
+        # Load the emotion model here to avoid reloading for each frame
+        self.emotion_model = self.load_emotion_model()
     
     def __del__(self):
         self.video.release()
-        
-    def get_frame(self):
-        ret, frame = self.video.read()
-        #all emotion output as a dictionary
-        emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
 
-        #load json model
+    def load_emotion_model(self):
         json_file = open('Model/emotion_model.json', 'r')
         loaded_model_json = json_file.read()
         json_file.close()
-        emotion_model = model_from_json(loaded_model_json)
-        emotion_model.load_weights("Model/emotion_model.h5")
-            
+        model = model_from_json(loaded_model_json)
+        model.load_weights("Model/emotion_model.h5")
+        return model
+        
+    def get_frame(self):
+        emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
         ret, frame = self.video.read()
+        if not ret:
+            return None, None
+
         frame = cv2.resize(frame, (1280, 720))
-        
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        
         num_faces = face_detector.detectMultiScale(gray_frame, scaleFactor=1.3, minNeighbors=5)
 
-        for (x, y, w, h) in num_faces:
-            cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (0, 255, 0), 4)
-            roi_gray_frame = gray_frame[y:y + h, x:x + w]
-            cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray_frame, (48, 48)), -1), 0) # model acceptable data
+        current_emotion = None
 
-            # predict the emotions
-            emotion_prediction = emotion_model.predict(cropped_img)
-            maxindex = int(np.argmax(emotion_prediction)) # get max likelihood (max percentage that a certain emotion is being displayed)
-            cv2.putText(frame, emotion_dict[maxindex], (x+5, y-20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA) # find the emotion from the dictionary
+        for (x, y, w, h) in num_faces:
+            roi_gray_frame = gray_frame[y:y + h, x:x + w]
+            cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray_frame, (48, 48)), -1), 0)
+
+            emotion_prediction = self.emotion_model.predict(cropped_img)
+            maxindex = int(np.argmax(emotion_prediction))
+            current_emotion = emotion_dict[maxindex]
+
+            cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (0, 255, 0), 4)
+            cv2.putText(frame, current_emotion, (x+5, y-20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
             
-            
-    
         ret, jpg = cv2.imencode('.jpg', frame)
-        return jpg.tobytes()
+        return jpg.tobytes(), current_emotion
